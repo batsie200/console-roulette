2439023b850faf21570d5fb6b75269ade79c4d7c
Rubatsiro Chiripa <rubatsirochiripa@gmail.com>
2020-08-24 10:40:33 +0200

    first commit

---
+++ src/main/java/com/rc/assessment/consoleroulette/ConsoleRouletteApplication.java
@@ -0,0 +1,13 @@
+package com.rc.assessment.consoleroulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleRouletteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsoleRouletteApplication.class, args);
+    }
+
+}
